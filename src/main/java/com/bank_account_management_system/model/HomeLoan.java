42b19110b41a6a4d038182a973d2d557735e4c47
Abdalrahman Walid <alramly1451@gmail.com>
2026-04-27 16:35:05 +0300

    implement fields and methods for LoanAcount

--- src/main/java/com/bank_account_management_system/model/HomeLoan.java
+++ src/main/java/com/bank_account_management_system/model/HomeLoan.java
@@ -1,4 +1,33 @@
 package com.bank_account_management_system.model;
 
-public class HomeLoan {
+public class HomeLoan extends LoanAccount {
+    private String propertyAddress;
+
+    public HomeLoan(int accountId, String holderName,
+                   double balance,double loanAmount, double remainingAmount ,String CarModel) {
+        super(accountId, holderName, balance, loanAmount, remainingAmount);
+        this.propertyAddress = CarModel;
+
+    }
+
+
+    String getPropertyAddress() {
+        return propertyAddress;
+    }
+    public void setPropertyAddress(String CarModel) {
+        this.propertyAddress = CarModel;
+    }
+
+
+    @Override
+    public void payInstallment(double amount)
+    {
+
+    }
+
+    @Override
+    public void applyMonthlyUpdate()
+    {
+
+    }
 }
--- src/main/java/com/bank_account_management_system/model/LoanAccount.java
+++ src/main/java/com/bank_account_management_system/model/LoanAccount.java
@@ -1,1 +1,26 @@
 package com.bank_account_management_system.model;
+
+public abstract class LoanAccount extends BankAccount {
+
+    private double loanAmount;
+    private double remainingAmount;
+
+    public LoanAccount(int accountId, String holderName,
+                       double balance,double loanAmount, double remainingAmount) {
+        super(accountId, holderName, balance);
+    }
+
+
+    abstract public void payInstallment(double amount);
+
+
+    //because it is a LoanAccount -> clint can't make withdraw
+    @Override
+    public boolean withdraw(double amount) {
+            return false;
+    }
+
+
+
+}
+
