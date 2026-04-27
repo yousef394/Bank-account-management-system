e11cabcb22e59c5e6a47d3cb86a98658681084f7
Abdalrahman Walid <alramly1451@gmail.com>
2026-04-27 15:39:09 +0300

    Creating class Transaction and implement its basic fields and methods

--- src/main/java/com/bank_account_management_system/model/Transaction.java
+++ src/main/java/com/bank_account_management_system/model/Transaction.java
@@ -1,1 +1,52 @@
 package com.bank_account_management_system.model;
+
+import java.time.LocalDate;
+
+public class Transaction implements Printable {
+    private int transactionId;
+    private TransactionType type;
+    private double amount;
+    private LocalDate date;
+
+    Transaction(int transactionId, TransactionType type, double amount, LocalDate date) {
+        this.transactionId = transactionId;
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+    }
+    public int getTransactionId() {
+        return transactionId;
+    }
+    public TransactionType getType() {
+        return type;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String printDetails()
+    {
+        return "Transaction Id: "+transactionId+
+                " Type: "+type+
+                " Amount: "+ amount+
+                " Date: "+date.toString();
+
+    }
+
+
+
+}
+
