1ab0989c9b62abbc6691fb5d0c5b4658d7f1b0b6
Abdalrahman Walid <alramly1451@gmail.com>
2026-04-27 16:36:35 +0300

    creating carloan and homeloan as childs for LoanAcount

--- src/main/java/com/bank_account_management_system/model/CarLoan.java
+++ src/main/java/com/bank_account_management_system/model/CarLoan.java
@@ -1,4 +1,33 @@
 package com.bank_account_management_system.model;
 
-public class CarLoan {
+public class CarLoan extends LoanAccount {
+    private String CarModel;
+
+    public CarLoan(int accountId, String holderName,
+                   double balance,double loanAmount, double remainingAmount ,String CarModel) {
+        super(accountId, holderName, balance, loanAmount, remainingAmount);
+        this.CarModel = CarModel;
+
+    }
+
+    String getCarModel() {
+        return CarModel;
+    }
+    public void setCarModel(String CarModel) {
+        this.CarModel = CarModel;
+    }
+
+
+    @Override
+    public void payInstallment(double amount)
+    {
+
+    }
+
+    @Override
+    public void applyMonthlyUpdate()
+    {
+
+    }
+
 }
