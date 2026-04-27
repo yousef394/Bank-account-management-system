d0e4aabfbcebab8726bb02e9e33d83ac0482b036
Abdalrahman Walid <alramly1451@gmail.com>
2026-04-27 14:29:53 +0300

    Adding monthlyUpdate and Fee if withbraw is overdraftLimit

--- src/main/java/com/bank_account_management_system/model/CheckingAccount.java
+++ src/main/java/com/bank_account_management_system/model/CheckingAccount.java
@@ -1,17 +1,70 @@
 package com.bank_account_management_system.model;
 
 public class CheckingAccount extends BankAccount {
-    private double overdraft;
+    private double overdraftLimit;
+    private double fee = 0;
 
     public CheckingAccount(int id ,String holderName,double balance, double overdraft) {
         super(id,holderName,balance);
-        this.overdraft = overdraft;
+        this.overdraftLimit = overdraft;
     }
-    public double getOverdraft() {
-        return overdraft;
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
     }
-    public void setOverdraft(double overdraft) {
-        this.overdraft = overdraft;
+
+    public void setOverdraftLimit(double overdraft) {
+        this.overdraftLimit = overdraft;
     }
-    
+
+    public double getFee() {
+        return fee;
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        if(amount>0) {
+            if (getBalance() >= amount) {
+                return super.withdraw(amount);
+            }
+            else if (overdraftLimit + getBalance() >= amount) {
+
+                fee = (amount - getBalance()) * 1.25;
+                super.withdraw(amount);
+                setAuditlog("there is a fee = " + fee);
+                return true;
+            } else
+                return false;
+
+        }
+        else
+            return false;
+        }
+
+    @Override
+    public void applyMonthlyUpdate() {
+          if (fee>0)
+
+          {
+              if( getBalance()>=fee ) {
+                  setBalance(getBalance() - fee);
+                  setAuditlog("fee applied :" + fee);
+                  fee = 0;
+              }
+
+              else
+              {
+                  setAuditlog("Your Balance is lower than "+fee);
+                  fee*=1.5;
+                  setAuditlog("the fee increased to "+fee);
+
+              }
+
+          }
+
+          else
+              setAuditlog("there is no fee");
+
+    }
+
 }
