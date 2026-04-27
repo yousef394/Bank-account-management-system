756129c43069811f459788ec8c411f7cacdb0ec4
Abdalrahman Walid <alramly1451@gmail.com>
2026-04-27 11:47:30 +0300

    Creating checking Acount and Adding its basic fields and methods

--- src/main/java/com/bank_account_management_system/model/CheckingAccount.java
+++ src/main/java/com/bank_account_management_system/model/CheckingAccount.java
@@ -1,1 +1,17 @@
 package com.bank_account_management_system.model;
+
+public class CheckingAccount extends BankAccount {
+    private double overdraft;
+
+    public CheckingAccount(int id ,String holderName,double balance, double overdraft) {
+        super(id,holderName,balance);
+        this.overdraft = overdraft;
+    }
+    public double getOverdraft() {
+        return overdraft;
+    }
+    public void setOverdraft(double overdraft) {
+        this.overdraft = overdraft;
+    }
+    
+}
