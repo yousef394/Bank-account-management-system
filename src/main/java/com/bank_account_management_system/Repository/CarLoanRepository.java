55cff3cff2684b16d909952fbd176643bf2cf80b
Abdalrahman Walid <alramly1451@gmail.com>
2026-05-07 14:52:51 +0300

    implement loan accounts

--- src/main/java/com/bank_account_management_system/Repository/CarLoanRepository.java
+++ src/main/java/com/bank_account_management_system/Repository/CarLoanRepository.java
@@ -1,4 +1,133 @@
 package com.bank_account_management_system.Repository;
 
+import com.bank_account_management_system.model.CarLoan;
+import com.bank_account_management_system.model.CheckingAccount;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class CarLoanRepository {
+    //Encapsulation for  method & data fields will not be used outside
+    private static String fileName="src/main/resources/Files/carLoan.txt";
+
+    private static CarLoan parse(String line) {
+        String[] lineArray = line.split(BankAccountRepository.separator);
+        //to avoid crash
+        if(lineArray.length<8)
+            return null;
+
+        return new CarLoan(Integer.parseInt(lineArray[0]),lineArray[1]
+                , LocalDateTime.parse(lineArray[2]),lineArray[3]
+                ,Double.parseDouble(lineArray[4])
+                ,Double.parseDouble(lineArray[5]) ,Double.parseDouble(lineArray[6]),lineArray[7]);
+
+    }
+
+    private static String format(CarLoan object) {
+        return BankAccountRepository.format( object.getAccountId(),object.getPassword(),object.getDateCreated()
+                ,object.getHolderName(), object.getBalance())
+                +BankAccountRepository.separator+ object.getLoanAmount()
+                +BankAccountRepository.separator+ object.getRemainingAmount()
+                +BankAccountRepository.separator+object.getCarModel();
+    }
+
+
+    public static ArrayList<CarLoan> getAllAccounts() {
+
+        ArrayList<String>fileData =  BankAccountRepository.loadDataWithoutSplit(fileName);
+        ArrayList<CarLoan> carLoans = new ArrayList<>();
+        CarLoan carLoan;
+        for(String line : fileData)
+        {
+            carLoan = parse(line);
+
+            if(carLoan!=null)
+                carLoans.add(carLoan);
+        }
+
+
+        return carLoans;
+    }
+
+    public static boolean add(CarLoan carLoan){
+        String line =format(carLoan);
+        return BankAccountRepository.uploadLineToFile( line,fileName);
+    }
+
+    public static CarLoan findById(int accountId) {
+        ArrayList<String>fileData =  BankAccountRepository.loadDataWithoutSplit(fileName);
+        CarLoan carLoan;
+        for(String line : fileData) {
+            carLoan = parse(line);
+
+            if(carLoan != null)
+                if (accountId == carLoan.getAccountId())
+                    return carLoan;
+        }
+        return null;
+    }
+
+    public static CarLoan findByIdAndPassword(int accountId, String password) {
+        ArrayList<String>fileData =  BankAccountRepository.loadDataWithoutSplit(fileName);
+        CarLoan carLoan;
+        for(String line : fileData) {
+            carLoan = parse(line);
+            //may be return null
+            if(carLoan != null)
+                if (accountId == carLoan.getAccountId() && password.equals(carLoan.getPassword()))
+                    return carLoan;
+        }
+        return null;
+    }
+
+    public static boolean delete(int accountId) {
+        boolean result = false;
+        ArrayList<CarLoan> carLoans = getAllAccounts();
+        //to clear file
+        BankAccountRepository.clearFile(fileName);
+
+        for(CarLoan carLoan : carLoans) {
+            //skip
+            if(carLoan.getAccountId() == accountId) {
+                result = true;
+                continue;
+            }
+            //fill file
+            BankAccountRepository.uploadLineToFile(format(carLoan),fileName);
+
+        }
+        return result;
+    }
+
+    public static boolean update(CarLoan carLoan) {
+
+        boolean result = false;
+        ArrayList<CarLoan> carLoans = getAllAccounts();
+        //to clear file
+        BankAccountRepository.clearFile(fileName);
+        for(CarLoan currentCarLoanAccount : carLoans) {
+            //update data
+            if(currentCarLoanAccount.getAccountId() == carLoan.getAccountId()) {
+                currentCarLoanAccount.setPassword(carLoan.getPassword());
+                currentCarLoanAccount.setHolderName(carLoan.getHolderName());
+                currentCarLoanAccount.setBalance(carLoan.getBalance());
+                currentCarLoanAccount.setLoanAmount(carLoan.getLoanAmount());
+                currentCarLoanAccount.setRemainingAmount(carLoan.getRemainingAmount());
+                currentCarLoanAccount.setCarModel(carLoan.getCarModel());
+
+                result = true;
+
+            }
+            //fill file
+            BankAccountRepository.uploadLineToFile(format(currentCarLoanAccount),fileName);
+
+        }
+        return result;
+
+
+    }
+
+    public static boolean isExist(int accountId) {
+        return BankAccountRepository.isExist(accountId,fileName);
+    }
 }
--- src/main/java/com/bank_account_management_system/Repository/HomeLoanRepository.java
+++ src/main/java/com/bank_account_management_system/Repository/HomeLoanRepository.java
@@ -1,4 +1,134 @@
 package com.bank_account_management_system.Repository;
 
+import com.bank_account_management_system.model.CarLoan;
+import com.bank_account_management_system.model.HomeLoan;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class HomeLoanRepository {
+    //Encapsulation for  method & data fields will not be used outside
+    private static String fileName="src/main/resources/Files/homeLoan.txt";
+
+    private static HomeLoan parse(String line) {
+        String[] lineArray = line.split(BankAccountRepository.separator);
+        //to avoid crash
+        if(lineArray.length<8)
+            return null;
+
+        return new HomeLoan(Integer.parseInt(lineArray[0]),lineArray[1]
+                , LocalDateTime.parse(lineArray[2]),lineArray[3]
+                ,Double.parseDouble(lineArray[4])
+                ,Double.parseDouble(lineArray[5]) ,Double.parseDouble(lineArray[6]),lineArray[7]);
+
+    }
+
+    private static String format(HomeLoan object) {
+        return BankAccountRepository.format( object.getAccountId(),object.getPassword(),object.getDateCreated()
+                ,object.getHolderName(), object.getBalance())
+                +BankAccountRepository.separator+ object.getLoanAmount()
+                +BankAccountRepository.separator+ object.getRemainingAmount()
+                +BankAccountRepository.separator+object.getPropertyAddress();
+    }
+
+
+    public static ArrayList<HomeLoan> getAllAccounts() {
+
+        ArrayList<String>fileData =  BankAccountRepository.loadDataWithoutSplit(fileName);
+        ArrayList<HomeLoan> homeLoans = new ArrayList<>();
+        HomeLoan homeLoan;
+        for(String line : fileData)
+        {
+            homeLoan = parse(line);
+
+            if(homeLoan!=null)
+                homeLoans.add(homeLoan);
+        }
+
+
+        return homeLoans;
+    }
+
+    public static boolean add(HomeLoan homeLoan){
+        String line =format(homeLoan);
+        return BankAccountRepository.uploadLineToFile( line,fileName);
+    }
+
+    public static HomeLoan findById(int accountId) {
+        ArrayList<String>fileData =  BankAccountRepository.loadDataWithoutSplit(fileName);
+        HomeLoan homeLoan;
+        for(String line : fileData) {
+            homeLoan = parse(line);
+
+            if(homeLoan != null)
+                if (accountId == homeLoan.getAccountId())
+                    return homeLoan;
+        }
+        return null;
+    }
+
+    public static HomeLoan findByIdAndPassword(int accountId, String password) {
+        ArrayList<String>fileData =  BankAccountRepository.loadDataWithoutSplit(fileName);
+        HomeLoan homeLoan;
+        for(String line : fileData) {
+            homeLoan = parse(line);
+            //may be return null
+            if(homeLoan != null)
+                if (accountId == homeLoan.getAccountId() && password.equals(homeLoan.getPassword()))
+                    return homeLoan;
+        }
+        return null;
+    }
+
+    public static boolean delete(int accountId) {
+        boolean result = false;
+        ArrayList<HomeLoan> homeLoans = getAllAccounts();
+        //to clear file
+        BankAccountRepository.clearFile(fileName);
+
+        for(HomeLoan homeLoan : homeLoans) {
+            //skip
+            if(homeLoan.getAccountId() == accountId) {
+                result = true;
+                continue;
+            }
+            //fill file
+            BankAccountRepository.uploadLineToFile(format(homeLoan),fileName);
+
+        }
+        return result;
+    }
+
+    public static boolean update(HomeLoan homeLoan) {
+
+        boolean result = false;
+        ArrayList<HomeLoan> homeLoans = getAllAccounts();
+        //to clear file
+        BankAccountRepository.clearFile(fileName);
+        for(HomeLoan currentHomeLoanAccount : homeLoans) {
+            //update data
+            if(currentHomeLoanAccount.getAccountId() == homeLoan.getAccountId()) {
+                currentHomeLoanAccount.setPassword(homeLoan.getPassword());
+                currentHomeLoanAccount.setHolderName(homeLoan.getHolderName());
+                currentHomeLoanAccount.setBalance(homeLoan.getBalance());
+                currentHomeLoanAccount.setLoanAmount(homeLoan.getLoanAmount());
+                currentHomeLoanAccount.setRemainingAmount(homeLoan.getRemainingAmount());
+                currentHomeLoanAccount.setPropertyAddress(homeLoan.getPropertyAddress());
+
+                result = true;
+
+            }
+            //fill file
+            BankAccountRepository.uploadLineToFile(format(currentHomeLoanAccount),fileName);
+
+        }
+        return result;
+
+
+    }
+
+    public static boolean isExist(int accountId) {
+        return BankAccountRepository.isExist(accountId,fileName);
+    }
+
 }
