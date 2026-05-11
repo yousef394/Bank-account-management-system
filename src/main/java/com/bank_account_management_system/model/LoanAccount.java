e36391ce86012e6780b2f12aa3e00a999a18de66
Abdalrahman Walid <alramly1451@gmail.com>
2026-05-11 11:23:23 +0300

    impement payInstallment and fix printDetails and fix commets and implement applyMonthlyUpdate

--- src/main/java/com/bank_account_management_system/model/LoanAccount.java
+++ src/main/java/com/bank_account_management_system/model/LoanAccount.java
@@ -7,11 +7,17 @@ public abstract class LoanAccount extends BankAccount {
     private double loanAmount;
     private double remainingAmount;
 
+    //=========constructors==============
+
+    //for new loan
     public LoanAccount(int accountId,String password, String holderName,
                        double balance,double loanAmount, double remainingAmount) {
         super(accountId,password, holderName, balance);
+        this.loanAmount = loanAmount;
+        this.remainingAmount = remainingAmount;
     }
 
+    //for existed loan
     public LoanAccount(int accountId, String password, LocalDateTime dateCreated, String holderName,
                        double balance, double loanAmount, double remainingAmount) {
         super(accountId,password, holderName, balance,dateCreated);
@@ -19,45 +25,62 @@ public abstract class LoanAccount extends BankAccount {
         this.remainingAmount = remainingAmount;
     }
 
+
+    //========= Getters =========
     public double getLoanAmount() {
         return loanAmount;
     }
-    public void setLoanAmount(double loanAmount) {
-        this.loanAmount = loanAmount;
-    }
     public double getRemainingAmount() {
         return remainingAmount;
+    }
+
+    //========= Setters =========
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
     }
     public void setRemainingAmount(double remainingAmount) {
         this.remainingAmount = remainingAmount;
     }
 
 
+    //=======Core Logic ========
 
-     public void payInstallment(double amount)
-    {
+     public boolean payInstallment(double amount) {
 
+        if(amount <= 0) { return false; }
 
+        if (remainingAmount < amount) { return false; }
+
+         if(amount > getBalance()) {
+
+             return false; }
+
+         setBalance(getBalance() - amount);
+         remainingAmount -= amount;
+        return true;
     }
-
-    @Override
-    public void applyMonthlyUpdate()
-    {
-        payInstallment(0);
-    }
-
-    public String printDetails()
-    {
-       return super.printDetails()
-               +" Loan Amount: "+loanAmount+" Remaining Amount: "+remainingAmount;
-    }
-
-
-    //because it is a LoanAccount -> clint can't make withdraw
+    
     @Override
     public boolean withdraw(double amount) {
-            return false;
+        return false;
     }
+
+    //=========Override methods========
+    @Override
+    public void applyMonthlyUpdate(){
+
+        // we assume that loan payment in year
+        payInstallment(loanAmount/12);
+    }
+
+    @Override
+    public String printDetails() {
+
+       return super.printDetails()
+               +" | Loan Amount: "+loanAmount+" | Remaining Amount: "+remainingAmount;
+    }
+
+    // Loan accounts do not support withdrawals
 
 
 }
